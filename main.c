5abac6d4e0871183ed14a6a248ed308bf6202cba
barkbuff <connect.arindamhore@gmail.com>
2025-12-17 01:28:32 +0530

    Refactor board rendering and agent initialization logic

--- main.c
+++ main.c
@@ -6,11 +6,36 @@
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 800
 
-#define BOARD_WIDTH 100
-#define BOARD_HEIGHT 100
+#define BOARD_WIDTH 10
+#define BOARD_HEIGHT 10
 
 #define CELL_WIDTH ((float)SCREEN_WIDTH / BOARD_WIDTH)
 #define CELL_HEIGHT ((float)SCREEN_HEIGHT / BOARD_HEIGHT)
+
+#define AGENTS_COUNT 5
+
+typedef enum {
+    DIR_RIGHT = 0,
+    DIR_DOWN,
+    DIR_LEFT,
+    DIR_UP
+} Dir;
+
+typedef struct {
+    int pos_x, pos_y;
+    Dir dir;
+    int hunger;
+    int health;
+} Agent;
+
+typedef enum {
+    ACTION_NOP = 0,
+    ACTION_STEP,
+    ACTION_EAT,
+    ACTION_ATTACK
+} Action;
+
+Agent agents[AGENTS_COUNT];
 
 int scc(int code)
 {
@@ -32,7 +57,7 @@ void *scp(void *ptr)
     return ptr;
 }
 
-void render_board(SDL_Renderer *renderer)
+void render_board_grid(SDL_Renderer *renderer)
 {
     scc(SDL_SetRenderDrawColor(renderer, 60, 60, 60, 255));
     for(int x = 1; x < BOARD_WIDTH; x++)
@@ -50,6 +75,35 @@ void render_board(SDL_Renderer *renderer)
                 SCREEN_WIDTH, y * CELL_HEIGHT
             ));
         }
+    }
+}
+
+int random_int_range(int low, int high)
+{
+    return rand() % (high - low) + low;
+}
+
+Dir random_dir(void)
+{
+    return (Dir)random_int_range(0, 4);
+}
+
+Agent random_agent(void)
+{
+    Agent agent = {0};
+    agent.pos_x = random_int_range(0, BOARD_WIDTH);
+    agent.pos_y = random_int_range(0, BOARD_HEIGHT);
+    agent.dir = random_dir();
+    agent.hunger = 100;
+    agent.health = 100;
+    return agent;
+}
+
+void init_agents(void)
+{
+    for(size_t i = 0; i < AGENTS_COUNT; i++)
+    {
+        agents[i] = random_agent();
     }
 }
 
@@ -85,7 +139,7 @@ int main(int argc, char *argv[])
 
         scc(SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255));
         scc(SDL_RenderClear(renderer));
-        render_board(renderer);
+        render_board_grid(renderer);
         SDL_RenderPresent(renderer);
 
     }
